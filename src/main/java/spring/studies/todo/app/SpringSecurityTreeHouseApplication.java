834c10f2aa1309f4b4f986c99a11c84169920b92
AugustoCalado <17462762+AugustoCalado@users.noreply.github.com>
2020-01-26 15:21:19 -0300

    Initial commit

---
+++ src/main/java/spring/studies/todo/app/SpringSecurityTreeHouseApplication.java
@@ -0,0 +1,13 @@
+package spring.studies.todo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTreeHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTreeHouseApplication.class, args);
+	}
+
+}
